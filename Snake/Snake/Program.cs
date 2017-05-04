9cec96fc6e52ca8e2fd321f3b95a9ee7ccd16b41
Manizha <m.mirzoeva96@mail.ru>
2017-05-04 21:41:46 +0300

    dars7

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,27 +9,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');            
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point(2,4,'#');           
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(0, 4, '#');
+            HorizontalLine line = new HorizontalLine(2, 10, 8, '+');
+            line.Draw();
 
-            Point p4 = new Point(2, 6, '&'); 
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p3);
-            pList.Add(p4);
-
-            foreach (Point i in pList)
-            {
-                i.Draw();
-            }
+            VerticalLine vline = new VerticalLine(2, 10, 2, '+');
+            vline.Draw();
 
             Console.ReadKey();
         }
-       
+
     }
 }
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+        public VerticalLine(int y1, int y2, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int i = y1; i < y2; i++)
+            {
+                Point p = new Point(x, i, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
